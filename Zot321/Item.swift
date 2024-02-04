a065c67ce35e0a4fb4148a7a26e0c5988a710617
JiayuanA <96093509+JiayuanA@users.noreply.github.com>
2024-02-03 17:50:46 -0800

    yes

---
+++ Zot321/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Zot321
+//
+//  Created by Wang on 2/3/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Zot321/Zot321App.swift
@@ -0,0 +1,32 @@
+//
+//  Zot321App.swift
+//  Zot321
+//
+//  Created by Wang on 2/3/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Zot321App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
